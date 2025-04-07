02e8eccb8ccfdc9b0edbaff50a838e21d33cdb31
Andrew DeVries <adevries@digitalexample.com>
2025-04-06 20:16:36 -0400

    fix upgrade sudo su radar

--- install/raspberrypi/raspberryPiUpgrade.sh
+++ install/raspberrypi/raspberryPiUpgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash     
 echo "Starting Raspberry Pi Upgrade Script"
 cd /opt/de/radar
-#sudu su radar
+sudu su radar
 echo downloading latest version of code
 curl -s https://api.github.com/repos/Andrewiski/RadarGun/releases/latest | sed -n 's/.*"tarball_url": "\(.*\)",.*/\1/p' | xargs -n1 wget -O - -q | tar -xz --strip-components=1
 npm install
