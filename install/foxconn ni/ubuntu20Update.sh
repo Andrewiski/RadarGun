4ad715e98936fc99717dcfb4e590233850c4fd83
Andrew DeVries <adevries@digitalexample.com>
2026-05-05 20:09:42 -0400

    fix outdated node.js packages

--- install/foxconn ni/ubuntu20Update.sh
+++ install/foxconn ni/ubuntu20Update.sh
@@ -4,8 +4,9 @@ mv /opt/de/radar /opt/de/radarOld # move the current radar directory to radarOld
 mkdir -p /opt/de/radar
 echo downloading latest version of code
 DOWNLOAD_URL=$(curl -s https://api.github.com/repos/Andrewiski/RadarGun/releases/latest | grep -o '"browser_download_url": *"[^"]*radargunmonitor\.zip"' | grep -o 'https://[^"]*')
-wget -q -O radargunmonitor.zip "$DOWNLOAD_URL"
-unzip -q radargunmonitor.zip
-rm radargunmonitor.zip
+wget -q -O /opt/de/radar/radargunmonitor.zip "$DOWNLOAD_URL"
+unzip -q /opt/de/radar/radargunmonitor.zip -d /opt/de/radar
+rm /opt/de/radar/radargunmonitor.zip
+cd /opt/de/radar
 npm install
 
